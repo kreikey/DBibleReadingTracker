33e03c9090675b8524c4ee141b1f4374d3ca8486
Rick Kreikebaum <kreikey@users.noreply.github.com>
2018-11-16 14:15:23 -0800

    Changed separate chapters and books arrays to Books[] struct array.

--- BibleReadingTracker.d
+++ BibleReadingTracker.d
@@ -14,153 +14,6 @@ import std.typecons;
 import std.format;
 import std.exception : assumeUnique;
 
-string[] books = [
-  "Genesis",
-  "Exodus",
-  "Leviticus",
-  "Numbers",
-  "Deuteronomy",
-  "Joshua",
-  "Judges",
-  "Ruth",
-  "1 Samuel",
-  "2 Samuel",
-  "1 Kings",
-  "2 Kings",
-  "1 Chronicles",
-  "2 Chronicles",
-  "Ezra",
-  "Nehemiah",
-  "Esther",
-  "Job",
-  "Psalms",
-  "Proverbs",
-  "Ecclesiastes",
-  "Song of Songs",
-  "Isaiah",
-  "Jeremiah",
-  "Lamentations",
-  "Ezekiel",
-  "Daniel",
-  "Hosea",
-  "Joel",
-  "Amos",
-  "Obadiah",
-  "Jonah",
-  "Micah",
-  "Nahum",
-  "Habakkuk",
-  "Zephaniah",
-  "Haggai",
-  "Zechariah",
-  "Malachi",
-  "Matthew",
-  "Mark",
-  "Luke",
-  "John",
-  "Acts",
-  "Romans",
-  "1 Corinthians",
-  "2 Corinthians",
-  "Galatians",
-  "Ephesians",
-  "Philippians",
-  "Colossians",
-  "1 Thessalonians",
-  "2 Thessalonians",
-  "1 Timothy",
-  "2 Timothy",
-  "Titus",
-  "Philemon",
-  "Hebrews",
-  "James",
-  "1 Peter",
-  "2 Peter",
-  "1 John",
-  "2 John",
-  "3 John",
-  "Jude",
-  "Revelation"
-];
-
-ulong[] chapters = [
-  50,
-  40,
-  27,
-  36,
-  34,
-  24,
-  21,
-  4,
-  31,
-  24,
-  22,
-  25,
-  29,
-  36,
-  10,
-  13,
-  10,
-  42,
-  150,
-  31,
-  12,
-  8,
-  66,
-  52,
-  5,
-  48,
-  12,
-  14,
-  3,
-  9,
-  1,
-  4,
-  7,
-  3,
-  3,
-  3,
-  2,
-  14,
-  4,
-  28,
-  16,
-  24,
-  21,
-  28,
-  16,
-  16,
-  13,
-  6,
-  6,
-  4,
-  4,
-  5,
-  3,
-  6,
-  4,
-  3,
-  1,
-  13,
-  5,
-  5,
-  3,
-  5,
-  1,
-  1,
-  1,
-  22
-];
-
-immutable ulong[string] idByBook;
-
-static this() {
-  auto temp = books.enumerate.map!(reverse).assocArray();
-  temp.rehash();
-  idByBook = assumeUnique(temp);
-  resetIeeeFlags();
-}
-
 struct BookRange {
   string firstBook;
   string lastBook;
@@ -203,7 +56,7 @@ struct BookRange {
         frontID++;
       }
 
-      string front() @property {
+      Book front() @property {
         return books[frontID];
       }
 
@@ -220,7 +73,6 @@ unittest {
   assert(isInputRange!(typeof(r.byID())));
   assert(isInputRange!(typeof(r.byBook())));
 }
-
 
 struct ChaptersDays {
   long chapters;
@@ -379,22 +231,22 @@ struct ReadingSection {
       .join();
 
     totalChapters = bookIDs
-      .map!(a => chapters[a])
+      .map!(i => books[i].chapters)
       .sum();
   } 
 
   string decodeChapterID(ulong chapterID) {
     auto chaptersBook = bookIDs
-      .map!(a => chapters[a])
+      .map!(i => books[i].chapters)
       .cumulativeFold!((a, b) => a + b)
       .zip(bookIDs)
       .find!(a => a[0] >= chapterID)
       .front;
 
     ulong bookID = chaptersBook[1];
-    ulong chapter = chapterID - (chaptersBook[0] - chapters[bookID]);
+    ulong chapter = chapterID - (chaptersBook[0] - books[bookID].chapters);
 
-    return format!"%s %d"(books[bookID], chapter);
+    return format!"%s %d"(books[bookID].name, chapter);
   }
 
   long encodeChapterID(string bookAndChapter) {
@@ -411,7 +263,7 @@ struct ReadingSection {
 
     return bookIDs
       .until(bookID)
-      .map!(a => chapters[a])
+      .map!(i => books[i].chapters)
       .sum() + chapter;
   }
 
@@ -488,7 +340,93 @@ struct ReadingSection {
 
     return Result(totalDays, multiplicity);
   }
+}
 
+struct Book {
+  string name;
+  ulong chapters;
+}
+
+static Book[] books = [
+  {"Genesis", 50},
+  {"Exodus", 40},
+  {"Leviticus", 27},
+  {"Numbers", 36},
+  {"Deuteronomy", 34},
+  {"Joshua", 24},
+  {"Judges", 21},
+  {"Ruth", 4},
+  {"1 Samuel", 31},
+  {"2 Samuel", 24},
+  {"1 Kings", 22},
+  {"2 Kings", 25},
+  {"1 Chronicles", 29},
+  {"2 Chronicles", 36},
+  {"Ezra", 10},
+  {"Nehemiah", 13},
+  {"Esther", 10},
+  {"Job", 42},
+  {"Psalms", 150},
+  {"Proverbs", 31},
+  {"Ecclesiastes", 12},
+  {"Song of Songs", 8},
+  {"Isaiah", 66},
+  {"Jeremiah", 52},
+  {"Lamentations", 5},
+  {"Ezekiel", 48},
+  {"Daniel", 12},
+  {"Hosea", 14},
+  {"Joel", 3},
+  {"Amos", 9},
+  {"Obadiah", 1},
+  {"Jonah", 4},
+  {"Micah", 7},
+  {"Nahum", 3},
+  {"Habakkuk", 3},
+  {"Zephaniah", 3},
+  {"Haggai", 2},
+  {"Zechariah", 14},
+  {"Malachi", 4},
+  {"Matthew", 28},
+  {"Mark", 16},
+  {"Luke", 24},
+  {"John", 21},
+  {"Acts", 28},
+  {"Romans", 16},
+  {"1 Corinthians", 16},
+  {"2 Corinthians", 13},
+  {"Galatians", 6},
+  {"Ephesians", 6},
+  {"Philippians", 4},
+  {"Colossians", 4},
+  {"1 Thessalonians", 5},
+  {"2 Thessalonians", 3},
+  {"1 Timothy", 6},
+  {"2 Timothy", 4},
+  {"Titus", 3},
+  {"Philemon", 1},
+  {"Hebrews", 13},
+  {"James", 5},
+  {"1 Peter", 5},
+  {"2 Peter", 3},
+  {"1 John", 5},
+  {"2 John", 1},
+  {"3 John", 1},
+  {"Jude", 1},
+  {"Revelation", 22}
+];
+
+immutable ulong[string] idByBook;
+
+static this() {
+  auto temp = books
+    .map!(b => b.name)
+    .enumerate
+    .map!(reverse)
+    .assocArray();
+  temp.rehash();
+  idByBook = assumeUnique(temp);
+  resetIeeeFlags();
 }
 
 void main(string[] args) {
